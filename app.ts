7a0d59bbfcd0763e38b4a474d3a5ca53735b377e
Diógenes Ribeiro de Oliveira <diogenes.riboliveira@gmail.com>
2022-09-09 11:35:21 -0300

    Creating a new ROUTE POST

--- app.ts
+++ app.ts
@@ -6,8 +6,13 @@ const app = express()
 app.use(express.json())
 app.use(express.static((`${__dirname}/public`)))
 
-app.get('/', ( req: Request, res: Response ) => {
-    res.render('index.html')
+app.get('/', (req: Request, res: Response) => {
+    res.render('index')
+})
+
+app.post('/', (req: Request, res: Response) => {
+    res.send(req.body)
+    console.log(req.body)
 })
 
 app.listen(PORT, () => {
