385e4d12488bca0b70a87a2cb6a85242f9aeb4e4
Diógenes Ribeiro de Oliveira <diogenes.riboliveira@gmail.com>
2022-10-02 18:12:27 -0300

    News validations

--- src/middlewares/taskCreateValidation.ts
+++ src/middlewares/taskCreateValidation.ts
@@ -8,7 +8,24 @@ export const taskCreateValidation = () => {
             .isString()
             .withMessage('O título é obrigatório')
             .isLength({  min: 5 })
-            .withMessage('O título precisa ter no mínimo 5 caracteres!')
+            .withMessage('O título precisa ter no mínimo 5 caracteres!'),
+
+        body('description')
+            .isString()
+            .withMessage('A descrição é obrigatória!')
+            .isLength({ min: 5 })
+            .withMessage('A descrição precisa ter no mínimo 5 caracteres!'),
+
+        body('difficulty')
+            .isNumeric()
+            .withMessage('O nível de dificuldade precisa ser um número')
+            .custom((value: number) => {
+                if(value < 0 || value > 10){
+                    throw new Error('O nível da dificuldade precisa ser entre 0 e 10')
+                }
+
+                return true
+            })
 
     ]
 
