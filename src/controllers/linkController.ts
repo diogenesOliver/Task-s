35e3e271ca73dc2631b9171ada070b39b0f2d0fa
Diógenes Ribeiro de Oliveira <diogenes.riboliveira@gmail.com>
2022-10-02 17:48:22 -0300

    Fcuntion controller

--- src/controllers/linkController.ts
+++ src/controllers/linkController.ts
@@ -1,5 +1,17 @@
 import { Response, Request } from "express"
+import { TaskModel } from '../model/Task'
 
-export async function returningScreen(req: Request, res: Response) {
-    res.send('API FUNCIONANDO ATÉ O MOMENTO!!')
+export async function createTask(req: Request, res: Response) {
+
+    try{
+
+        const data = await req.body
+        const task = TaskModel.create(data)
+
+        return res.status(200).json(task)
+
+    }catch(e: any){
+        return res.status(404).json({ error: 'Houve um erro tente novamente mais tarde!' })
+    }
+
 }
