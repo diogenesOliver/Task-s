6df7e239b74f5c99902734a3dfc453e27388427a
Diógenes Ribeiro de Oliveira <diogenes.riboliveira@gmail.com>
2022-10-04 18:37:08 +0000

    Filtering task by difficulty

--- src/controllers/linkController.ts
+++ src/controllers/linkController.ts
@@ -19,6 +19,17 @@ export async function createTask(req: Request, res: Response) {
 
 export async function filteringTask(req: Request, res: Response){
 
-    
+    try{
+
+        const difficultyLevel = req.params
+        const task = await TaskModel.find(difficultyLevel)
+
+        return res.status(200).json(task)
+
+    }catch(e: any){
+
+        return res.status(500).json({ e: "Houve um erro! Tente novamente mais tarde!" }) 
+
+    }
 
 }
