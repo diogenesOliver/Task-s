74919d79f3f8e8b626a4ae9a4b542a440a9634a8
Diógenes Ribeiro de Oliveira <diogenes.riboliveira@gmail.com>
2022-10-04 18:37:37 +0000

    Import new feature

--- src/router.ts
+++ src/router.ts
@@ -1,9 +1,10 @@
 import { Router, Request, Response } from 'express'
-import { createTask } from './controllers/linkController'
+import { createTask, filteringTask } from './controllers/linkController'
 import { taskCreateValidation } from './middlewares/taskCreateValidation'
 
 import { validate } from './middlewares/handleValidation'
 
 const router = Router()
 
-export default router.post('/teste', taskCreateValidation(), validate, createTask)
+export default router.post('/create', taskCreateValidation(), validate, createTask)
+    .get('/find-task/:difficulty', filteringTask)
