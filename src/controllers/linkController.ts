98baca3cec16d46f2a9a3207744395c302240e60
Diógenes Ribeiro de Oliveira <diogenes.riboliveira@gmail.com>
2022-10-06 09:55:46 -0300

    New feature

--- src/controllers/linkController.ts
+++ src/controllers/linkController.ts
@@ -72,3 +72,25 @@ export async function removeTask(req: Request, res: Response){
     }
 
 }
+
+export async function updateTask(req: Request, res: Response){
+
+    try{
+
+        const id = req.params.id
+        const data = req.body
+        const task = await TaskModel.findById(id)
+
+        if(!task){
+            return res.status(404).json({ error: 'Essa task não existe' })
+        }
+
+        await TaskModel.updateOne({ id: id }, data)
+
+        return res.status(200).send(data)
+
+    }catch(e: any){
+        return res.status(500).json({ e: "Houve um erro! tente novamente mais tarde!" })
+    }
+
+}
