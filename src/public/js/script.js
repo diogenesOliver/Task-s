1532541b693c688d6c34a30a2accd089ee24bccd
Diógenes Ribeiro de Oliveira <diogenes.riboliveira@gmail.com>
2022-10-06 18:26:53 +0000

    Retorning datas

--- src/public/js/script.js
+++ src/public/js/script.js
@@ -1,13 +1,17 @@
 async function apiTeste(){
 
-    fetch('/api/all-tasks')
-        .then(res => {
+    fetch('/api/all-tasks').then(res => {
 
-            console.log(res)
+        res.json().then(data => {
 
-        }).catch(err => {
-            console.log(err)
-        })
+            console.log(data)
+
+        }).catch(err => { console.log(err) })
+
+    }).catch(e => {
+        console.log(e)
+    })
+        
 
 }
 
