e69c57a34afbe93f9cbc4e749f0bdf022c2eb2f8
Diógenes Ribeiro de Oliveira <diogenes.riboliveira@gmail.com>
2022-10-07 17:57:34 +0000

    Importing Logger and new LOGS

--- src/main.ts
+++ src/main.ts
@@ -12,13 +12,13 @@ app.use(express.json())
 app.use(express.static((`${__dirname}/public`)))
 
 import db from '../config/db'
-
 import router from './router'
+import Logger from '../config/logger'
 
 app.use('/api/', router)
 
 app.listen(PORT, async () => {
     await db()
 
-    console.log(`Server runnning PORT ${PORT}`)
+    Logger.info(`Server runnning PORT ${PORT}`)
 })
