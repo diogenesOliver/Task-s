8052c36b69c5fa0f0f03b2496911ba31bd3f285f
Diógenes Ribeiro de Oliveira <diogenes.riboliveira@gmail.com>
2022-10-07 18:03:36 +0000

    Fixing the bug on LOGGER

--- config/default.ts
+++ config/default.ts
@@ -1,6 +1,7 @@
 export default{
 
     port: 3000,
-    dbUri: "mongodb+srv://diogenesOliver:kifYi8sL7iSDlH80@cluster0.aqkhdy3.mongodb.net/?retryWrites=true&w=majority"
+    dbUri: "mongodb+srv://diogenesOliver:kifYi8sL7iSDlH80@cluster0.aqkhdy3.mongodb.net/?retryWrites=true&w=majority",
+    env: "development"
 
 }
