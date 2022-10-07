474d08eeca559108fac605172e74196b541f8448
Diógenes Ribeiro de Oliveira <diogenes.riboliveira@gmail.com>
2022-10-07 18:15:43 +0000

    Setting PORT of server

--- config/default.ts
+++ config/default.ts
@@ -1,6 +1,6 @@
 export default{
 
-    port: 3000,
+    port: 8080,
     dbUri: "mongodb+srv://diogenesOliver:kifYi8sL7iSDlH80@cluster0.aqkhdy3.mongodb.net/?retryWrites=true&w=majority",
     env: "development"
 
