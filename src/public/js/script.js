c0ee6a19de296a947c3783da30f4e7fee5720e06
Diógenes Ribeiro de Oliveira <diogenes.riboliveira@gmail.com>
2022-10-11 17:41:35 +0000

    Removing comments

--- src/public/js/script.js
+++ src/public/js/script.js
@@ -1,22 +1,3 @@
-/* async function apiTeste(){
-
-    fetch('/api/all-tasks').then(res => {
-
-        res.json().then(data => {
-
-            console.log(data)
-
-        }).catch(err => { console.log(err) })
-
-    }).catch(e => {
-        console.log(e)
-    })
-
-
-}
-
-apiTeste() */
-
 const divElement = document.getElementsByClassName('div-teste')
 
 async function consumindoApi() {
