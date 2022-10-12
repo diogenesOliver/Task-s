05a9cd10315f25cb9ac56137f731e3d7244f28e0
Diógenes Ribeiro de Oliveira <diogenes.riboliveira@gmail.com>
2022-10-12 11:38:11 -0300

    View engine EJS

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,17 @@ require('dotenv').config()
 
 import express from 'express'
 import config from 'config'
+import path from 'path'
 
 const app = express()
 
 const PORT = process.env.PORT || config.get<number>('port')
 
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'public'))
+
 app.use(express.json())
-app.use(express.static((`${__dirname}/public`)))
+app.use('/api', express.static(path.join(__dirname, 'public')))
 
 import db from '../config/db'
 import router from './router'
