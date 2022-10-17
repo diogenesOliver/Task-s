afae161633f89d5dc07f6d54ffde3ff0dd72702a
Diógenes Ribeiro de Oliveira <diogenes.riboliveira@gmail.com>
2022-10-17 18:01:54 +0000

    Importing de new functio

--- src/router.ts
+++ src/router.ts
@@ -1,17 +1,16 @@
 import { Router, Request, Response } from 'express'
-import { createTask, filteringTask, getAllTasks, removeTask, updateTask } from './controllers/linkController'
+import { createTask, filteringTask, getAllTasks, removeTask, updateTask, renderHomePage } from './controllers/linkController'
 import { taskCreateValidation } from './middlewares/taskCreateValidation'
 
 import { validate } from './middlewares/handleValidation'
 
 const router = Router()
 
-export default router.post('/create', taskCreateValidation(), validate, createTask)
+export default router
+    .get('/home', renderHomePage)
+    .post('/create', taskCreateValidation(), validate, createTask)
     .get('/find-task/:difficulty', filteringTask)
     .get('/all-tasks', getAllTasks)
     .delete('/remove-task/:id', removeTask)
     .patch('/update-task/:id', taskCreateValidation(), validate, updateTask)
     .get('/find-task/:title')
-    .get('/home', (req: Request, res: Response) => {
-        res.render('index.ejs')
-    })
