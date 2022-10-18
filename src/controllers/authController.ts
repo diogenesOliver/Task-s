1e3816c5242bc2e59a8a28a45aefff383ebf61d0
Diógenes Ribeiro de Oliveira <diogenes.riboliveira@gmail.com>
2022-10-18 09:32:59 -0200

    Testing function

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,4 +1,13 @@
 import { UserModel } from "../model/User";
-import * as bcrypt from 'bcrypt';
+import { Request, Response } from 'express'
+
+import * as bcrypt from 'bcrypt'
 import * as jwt from 'jsonwebtoken'
 
+export async function functionTeste(req: Request, res: Response){
+
+    const dataJson = req.body
+
+    res.send(dataJson)
+    console.log(dataJson)
+}
