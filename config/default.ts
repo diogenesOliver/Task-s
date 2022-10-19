8fd2ea4347e0719f1c0807578064c9837e47405d
Diógenes Ribeiro de Oliveira <diogenes.riboliveira@gmail.com>
2022-10-19 16:49:51 +0000

    Hiding user information from  database

--- config/default.ts
+++ config/default.ts
@@ -1,7 +1,10 @@
+const dbUser = process.env.DB_USER
+const dbPassword = process.env.DB_PASSWORD
+
 export default{
 
     port: 8080,
-    dbUri: "mongodb+srv://diogenesOliver:kifYi8sL7iSDlH80@cluster0.aqkhdy3.mongodb.net/?retryWrites=true&w=majority",
+    dbUri: `mongodb+srv://${dbUser}:${dbPassword}@cluster0.aqkhdy3.mongodb.net/?retryWrites=true&w=majority`,
     env: "development"
 
 }
