ccdbd3e50caab9dfc33c0f4e14bb794cd5955b16
Diógenes Ribeiro de Oliveira <diogenes.riboliveira@gmail.com>
2022-10-20 18:29:33 +0000

    New validations to created a new Users

--- src/middlewares/userCreateValidation.ts
+++ src/middlewares/userCreateValidation.ts
@@ -8,23 +8,32 @@ export const userCreateValidation = () => {
             .isString()
             .withMessage('O email inválido')
             .isLength({ min: 20 })
-            .withMessage('O email informado é curto de mais'),
+            .withMessage('O email informado é curto de mais')
+            .isEmpty()
+            .withMessage("O email é obrigatório"),
 
         body('password')
             .isString()
             .withMessage('A senha deve ter no mínimo dois caracteres especiais')
             .isLength({ min: 10 })
-            .withMessage('A senha informada é curta de mais'),
+            .withMessage('A senha informada é curta de mais')
+            .isEmpty()
+            .withMessage("A senha é obrigatória"),
 
         body('name')
             .isString()
             .withMessage('Nome inválido')
             .isLength({ min: 5 })
-            .withMessage('O nome informado é curto de mais'),
+            .withMessage('O nome informado é curto de mais')
+            .isEmpty()
+            .withMessage("O nome é obrigatório"),
+            
 
         body('age')
             .isNumeric()
             .withMessage('A idade informada é inválida')
+            .isEmpty()
+            .withMessage("A idade é obrigatória"),
     ]
 
 }
