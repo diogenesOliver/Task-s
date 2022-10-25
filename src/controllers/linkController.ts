60177d984028a2ec406f2a5051f08cc0614e438c
Diógenes Ribeiro de Oliveira <diogenes.riboliveira@gmail.com>
2022-10-25 17:13:27 +0000

    Changing card state

--- src/controllers/linkController.ts
+++ src/controllers/linkController.ts
@@ -122,3 +122,28 @@ export async function findTaskByTitle(req: Request, res: Response){
 export async function renderHomePage(req: Request, res: Response){
     res.render('index.ejs')
 }
+
+export async function changingCardStatus(req: Request, res: Response){
+
+    try{
+
+        let { status } = req.body
+        const taskId = req.params.id
+        const task = await TaskModel.findById(taskId)
+
+        if(!task)
+            return res.status(500).send('Houve um erro tente novamente mais tarde!')
+
+        status = true
+        await TaskModel.updateOne({ id: taskId }, status)
+        
+        return res.status(200).send('Card atualizado com sucesso!')
+
+    }catch(e: any){
+
+        Logger.error(`Error on System: ${e.message}`)
+        return res.status(500).json({ e: "Houve um erro! Tente novamente mais tarde!" })
+
+    }
+
+}
