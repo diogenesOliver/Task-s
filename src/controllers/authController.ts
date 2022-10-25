29e7da28cb21fbe3f59286b230888767f74ce735
Diógenes Ribeiro de Oliveira <diogenes.riboliveira@gmail.com>
2022-10-25 10:59:46 -0200

    Creating athenticate user function

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -60,3 +60,14 @@ export async function getAllUsers(req: Request, res: Response){
     }
 
 }
+
+export async function athenticateUser(req: Request, res: Response){
+
+    const { email, password } = req.body
+
+    const user = await UserModel.findOne({ email }).select('+password')
+
+    if(!user)
+        return res.status(404).send({ error: 'User não encontrado!' })
+    
+}
