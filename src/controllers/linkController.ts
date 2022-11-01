e183f662b485c417889c67eef8f031e8e0b77d13
Diógenes Ribeiro de Oliveira <diogenes.riboliveira@gmail.com>
2022-11-01 19:54:00 -0200

    Filtering tasks completed an uncompleted

--- src/controllers/linkController.ts
+++ src/controllers/linkController.ts
@@ -147,3 +147,39 @@ export async function changingCardStatus(req: Request, res: Response){
     }
 
 }
+
+export async function gettingAllTasksCompleted(req: Request, res: Response){
+
+    try{
+
+        const statusTask = true
+        const task = await TaskModel.find({ status: statusTask })
+
+        return res.status(200).send(task)
+
+    }catch(e: any){
+
+        Logger.error(`Error on System: ${e.message}`)
+        return res.status(500).json({ e: "Houve um erro! Tente novamente mais tarde!" })
+
+    }
+
+}
+
+export async function gettingAllUncompletedTasks(req: Request, res: Response){
+
+    try{
+
+        const statusTask = false
+        const task = await TaskModel.find({ status: statusTask })
+
+        return res.status(200).send(task)
+
+    }catch(e: any){
+
+        Logger.error(`Error on System: ${e.message}`)
+        return res.status(500).json({ e: "Houve um erro! Tente novamente mais tarde!" })
+
+    }
+
+}
