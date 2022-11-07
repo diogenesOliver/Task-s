42a158dd1bf1f58716379696dd4cd21972fc0662
Diógenes Ribeiro de Oliveira <diogenes.riboliveira@gmail.com>
2022-11-07 17:10:38 +0000

    Getting options values

--- src/public/js/taskFilter.js
+++ src/public/js/taskFilter.js
@@ -2,7 +2,21 @@ const urlAPIByCompletedTasks = '/api/task-completed'
 const urlAPIByUncompletedTasks = '/api/task-uncompleted'
 const urlAPIByAllTasks = '/api/all-tasks'
 
-async function gettingTasksCompleted(){
+const buttonFilter = document.getElementById('buttonFilter')
+
+const allTaks = document.getElementById('allTasks')
+const taskConcluded = document.getElementById('taskConcluded')
+const unconcludedTask = document.getElementById('unconcludedTask')
+
+buttonFilter.addEventListener('click', () => {
+
+    console.log(allTaks.value)
+    console.log(taskConcluded.value)
+    console.log(unconcludedTask.value)
+
+})
+
+/* async function gettingTasksCompleted(){
 
     const res = await fetch(urlAPIByUncompletedTasks)
     const data = await res.json()
@@ -11,4 +25,4 @@ async function gettingTasksCompleted(){
 
 }
 
-gettingTasksCompleted()
+gettingTasksCompleted() */
