6e560d58c102dd4fd518aec62a98c50884bd89cd
Diógenes Ribeiro de Oliveira <diogenes.riboliveira@gmail.com>
2022-11-14 12:44:27 -0200

    Importing body-parser package

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,8 @@ import express from 'express'
 import config from 'config'
 import path from 'path'
 
+import * as bodyParser from 'body-parser'
+
 const app = express()
 
 const PORT = process.env.PORT || config.get<number>('port')
@@ -11,6 +13,7 @@ const PORT = process.env.PORT || config.get<number>('port')
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'public/pages'))
 
+app.use(bodyParser.urlencoded({ extended: true }))
 app.use(express.json())
 app.use('/api', express.static(path.join(__dirname, 'public')))
 
