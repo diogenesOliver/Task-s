65489594d788e8618ec53f665ed365eee21f2651
Diógenes Ribeiro de Oliveira <diogenes.riboliveira@gmail.com>
2022-11-20 18:45:02 -0200

    Removing somes validations

--- src/middlewares/userCreateValidation.ts
+++ src/middlewares/userCreateValidation.ts
@@ -14,18 +14,7 @@ export const userCreateValidation = () => {
             .isString()
             .withMessage('A senha deve ter no mínimo dois caracteres especiais')
             .isLength({ min: 10 })
-            .withMessage('A senha informada é curta de mais'),
-
-        body('name')
-            .isString()
-            .withMessage('Nome inválido')
-            .isLength({ min: 5 })
-            .withMessage('O nome informado é curto de mais'),
-            
-
-        body('age')
-            .isNumeric()
-            .withMessage('A idade informada é inválida')
+            .withMessage('A senha informada é curta de mais')
     ]
 
 }
