0aa739d26804919bb6a1cd10da63bf742bc2cf40
Diógenes Ribeiro de Oliveira <diogenes.riboliveira@gmail.com>
2022-11-22 20:15:29 -0200

    Logic for dark screen by open modal created

--- src/public/js/modalAdd.js
+++ src/public/js/modalAdd.js
@@ -1,28 +1,37 @@
 const buttonAdd = document.getElementById('buttonAdd')
-
 const buttonCloseModal = document.getElementById('closeModal')
 const cancelButton = document.getElementById('cancelButton')
 
 const modalAdd = document.getElementById('modalAdd')
+const modalWrapper = document.getElementById('modalWrapper')
 
 function openModal() {
-
     modalAdd.style.display = 'block'
-
 }
 
 function closeModal() {
     modalAdd.style.display = 'none'
 }
 
+function darkenScreenByOpenModal(darkScreen){
+    darkScreen.classList.add('modal-wrapper')
+}
+
+function removingDarkTheScreenByCloseModal(removeDarkScreen){
+    removeDarkScreen.classList.remove('modal-wrapper')
+}
+
 buttonCloseModal.addEventListener('click', () => {
     closeModal()
+    removingDarkTheScreenByCloseModal(modalWrapper)
 })
 
 cancelButton.addEventListener('click', () => {
     closeModal()
+    removingDarkTheScreenByCloseModal(modalWrapper)
 })
 
 buttonAdd.addEventListener('click', () => {
     openModal()
+    darkenScreenByOpenModal(modalWrapper)
 })
