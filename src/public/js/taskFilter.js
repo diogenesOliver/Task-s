55c1f8d0c981d5c3d1375943143986fc94111fac
Diógenes Ribeiro de Oliveira <diogenes.riboliveira@gmail.com>
2022-11-23 11:21:10 -0200

    Styling dropDown to filter tasks

--- src/public/js/taskFilter.js
+++ src/public/js/taskFilter.js
@@ -4,25 +4,3 @@ const urlAPIByAllTasks = '/api/all-tasks'
 
 const buttonFilter = document.getElementById('buttonFilter')
 
-const allTaks = document.getElementById('allTasks')
-const taskConcluded = document.getElementById('taskConcluded')
-const unconcludedTask = document.getElementById('unconcludedTask')
-
-buttonFilter.addEventListener('click', () => {
-
-    console.log(allTaks.value)
-    console.log(taskConcluded.value)
-    console.log(unconcludedTask.value)
-
-})
-
-/* async function gettingTasksCompleted(){
-
-    const res = await fetch(urlAPIByUncompletedTasks)
-    const data = await res.json()
-
-    console.log(data)
-
-}
-
-gettingTasksCompleted() */
