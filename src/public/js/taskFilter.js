c3948ebbcd5b487a46b494e9d964134701bff761
Diógenes Ribeiro de Oliveira <diogenes.riboliveira@gmail.com>
2022-11-24 11:01:03 -0200

    Creating logic to filter

--- src/public/js/taskFilter.js
+++ src/public/js/taskFilter.js
@@ -4,3 +4,25 @@ const urlAPIByAllTasks = '/api/all-tasks'
 
 const buttonFilter = document.getElementById('buttonFilter')
 
+function dropdownFunction(p) {
+
+    let elementInput = document.getElementsByClassName('dropDown')[0]
+    let stylingAttributes = ['block', 'none']
+
+    elementInput.style.display = stylingAttributes[p]
+
+    let dropDownTransition = ['0px', '0px, -10px']
+
+    setTimeout(() => {
+        elementInput.style.transform = `translate(${dropDownTransition[p]})`
+    }, 0)
+
+}
+
+function filteringTask(p) {
+
+    let gettingValueOfItem = document.getElementById('item-' + p).innerHTML
+
+    document.getElementsByTagName('input')[0].value = gettingValueOfItem
+
+}
