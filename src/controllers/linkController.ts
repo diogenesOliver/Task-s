9d4e4b2b4a62a53e1eee6eec2c7f47b9b82c3124
Diógenes Ribeiro de Oliveira <diogenes.riboliveira@gmail.com>
2022-12-08 19:21:26 -0200

    Removing function by get all tasks

--- src/controllers/linkController.ts
+++ src/controllers/linkController.ts
@@ -3,41 +3,6 @@ import { TaskModel } from '../model/Task'
 
 import Logger from '../../config/logger'
 
-/* export async function filteringTask(req: Request, res: Response){
-
-    try{
-
-        const difficultyLevel = req.params
-        const task = await TaskModel.find(difficultyLevel)
-
-        return res.status(200).json(task)
-
-    }catch(e: any){
-
-        Logger.error(`Error on System: ${e.message}`)
-        return res.status(500).json({ e: "Houve um erro! Tente novamente mais tarde!" }) 
-
-    }
-
-} */
-
-export async function getAllTasks(req: Request, res: Response){
-
-    try{
-        
-        const task = await TaskModel.find()
-
-        return res.status(200).json(task)
-
-    }catch(e: any){
-
-        Logger.error(`Error on System: ${e.message}`)
-        return res.status(500).json({ e: "Houve um erro! tente novamente mais tarde!" })
-
-    }
-
-}
-
 export async function removeTask(req: Request, res: Response){
 
     try{
