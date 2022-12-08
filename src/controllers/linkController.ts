4fc5ce60ab3523a9f132bff21958d8e9ae93229f
Diógenes Ribeiro de Oliveira <diogenes.riboliveira@gmail.com>
2022-12-08 19:09:57 -0200

    Removing function by filter task

--- src/controllers/linkController.ts
+++ src/controllers/linkController.ts
@@ -3,23 +3,7 @@ import { TaskModel } from '../model/Task'
 
 import Logger from '../../config/logger'
 
-/* export async function createTask(req: Request, res: Response) {
-
-    try{
-
-        const data = req.body
-        const task = await TaskModel.create(data)
-
-        return res.status(200).redirect('/api/home')
-
-    }catch(e: any){
-        Logger.error(`Error on System: ${e.message}`)
-        return res.status(500).json({ error: 'Houve um erro tente novamente mais tarde!' })
-    }
-
-} */
-
-export async function filteringTask(req: Request, res: Response){
+/* export async function filteringTask(req: Request, res: Response){
 
     try{
 
@@ -35,7 +19,7 @@ export async function filteringTask(req: Request, res: Response){
 
     }
 
-}
+} */
 
 export async function getAllTasks(req: Request, res: Response){
 
