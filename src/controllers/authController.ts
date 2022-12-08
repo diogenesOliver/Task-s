c0ebbef0301faa361496812fde1db3066250fdcd
Diógenes Ribeiro de Oliveira <diogenes.riboliveira@gmail.com>
2022-12-08 10:59:33 -0200

    Creating class by authenticate user

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,11 +1,6 @@
-import { UserModel } from "../model/User";
 import { Request, Response } from 'express'
 
 import * as authConfig from '../../config/authConfig.json'
-
-import Logger from '../../config/logger'
-
-import * as bcrypt from 'bcrypt'
 import * as jwt from 'jsonwebtoken'
 
 function generateToken(params = {}){
@@ -15,19 +10,6 @@ function generateToken(params = {}){
     })
 
 }
-
-export async function athenticateUser(req: Request, res: Response){
-
-    const { email, password } = req.body
-
-    const user = await UserModel.findOne({ email }).select('+password')
-
-    if(!user)
-        return res.status(404).send({ error: 'User não encontrado!' })
-    
-}
-
-/* Render pages */
 
 export async function renderSignUpPage(req: Request, res: Response) {
     res.render('signUpPage.ejs')
