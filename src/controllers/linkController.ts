deebb498b0237ae305f1d2a6d805abbdffaee6a8
Diógenes Ribeiro de Oliveira <diogenes.riboliveira@gmail.com>
2022-12-09 11:22:05 -0200

    Removing function by update task

--- src/controllers/linkController.ts
+++ src/controllers/linkController.ts
@@ -3,29 +3,6 @@ import { TaskModel } from '../model/Task'
 
 import Logger from '../../config/logger'
 
-export async function updateTask(req: Request, res: Response){
-
-    try{
-
-        const id = req.params.id
-        const data = req.body
-        const task = await TaskModel.findById(id)
-
-        if(!task){
-            return res.status(404).json({ error: 'Essa task não existe' })
-        }
-
-        await TaskModel.updateOne({ id: id }, data)
-
-        return res.status(200).send(data)
-
-    }catch(e: any){
-        Logger.error(`Error on System: ${e.message}`)
-        return res.status(500).json({ e: "Houve um erro! tente novamente mais tarde!" })
-    }
-
-}
-
 export async function findTaskByTitle(req: Request, res: Response){
 
     try{
