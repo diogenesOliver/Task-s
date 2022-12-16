43ad57a18f424a4a0d793f21935f19a9c2e213e6
Diógenes Ribeiro de Oliveira <diogenes.riboliveira@gmail.com>
2022-12-16 11:19:10 -0200

    Fixing the identation imports

--- src/router.ts
+++ src/router.ts
@@ -1,23 +1,23 @@
 import { Router } from 'express'
 
-import { createTaskUseCase } from './UseCases/CreateTask/createTaskIndex'
-import { taskFilterUseCase } from './UseCases/TaskFilter/taskFilterIndex'
-import { getAllTasksUseCase } from './UseCases/GetAllTasks/getTaksIndex'
-import { removeTaskUseCase } from './UseCases/RemoveTask/removeTaskIndex'
-import { updateTaskUseCase } from './UseCases/UpdateTask/updateTaskIndex'
-import { changeStatusTaskUseCase } from './UseCases/ChangeStatusTask/changeStatusIndex'
-import { completedTaksUseCase } from './UseCases/CompletedTasks/completedTasksIndex'
-import { incompleteTasksUseCase } from './UseCases/IncompleteTasks/incompleteTasksIndex'
+import { createTaskUseCase }        from './UseCases/CreateTask/createTaskIndex'
+import { taskFilterUseCase }        from './UseCases/TaskFilter/taskFilterIndex'
+import { getAllTasksUseCase }       from './UseCases/GetAllTasks/getTaksIndex'
+import { removeTaskUseCase }        from './UseCases/RemoveTask/removeTaskIndex'
+import { updateTaskUseCase }        from './UseCases/UpdateTask/updateTaskIndex'
+import { changeStatusTaskUseCase }  from './UseCases/ChangeStatusTask/changeStatusIndex'
+import { completedTaksUseCase }     from './UseCases/CompletedTasks/completedTasksIndex'
+import { incompleteTasksUseCase }   from './UseCases/IncompleteTasks/incompleteTasksIndex'
 
-import { createUserUseCase } from './UseCases/CreateUser/userIndex'
-import { authenticateUserUseCase } from './UseCases/AuthenticateUser/authUserIndex'
-import { gettingAllUserUseCase } from './UseCases/GetAllUsers/allUsersIndex'
+import { createUserUseCase }        from './UseCases/CreateUser/userIndex'
+import { authenticateUserUseCase }  from './UseCases/AuthenticateUser/authUserIndex'
+import { gettingAllUserUseCase }    from './UseCases/GetAllUsers/allUsersIndex'
 
-import { renderPageUseCase } from './UseCases/RenderPage/renderPageIndex'
+import { renderPageUseCase }        from './UseCases/RenderPage/renderPageIndex'
 
-import { taskCreateValidation } from './middlewares/taskCreateValidation'
-import { userCreateValidation } from './middlewares/userCreateValidation'
-import { validate } from './middlewares/handleValidation'
+import { taskCreateValidation }     from './middlewares/taskCreateValidation'
+import { userCreateValidation }     from './middlewares/userCreateValidation'
+import { validate }                 from './middlewares/handleValidation'
 
 const router = Router()
 
