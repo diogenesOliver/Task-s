7670141687151ff2b5e7cba6cd8326db85d1c80a
Diógenes Ribeiro de Oliveira <diogenes.riboliveira@gmail.com>
2022-12-15 22:07:14 -0200

    Importgin new class

--- src/main.ts
+++ src/main.ts
@@ -4,27 +4,29 @@ import express from 'express'
 import config from 'config'
 import path from 'path'
 
+import router from './router'
+import Logger from '../config/logger'
+
+import { connectDB } from "../config/db";
+
+const PORT = process.env.PORT || config.get<number>('port')
+
 import * as bodyParser from 'body-parser'
 
 const app = express()
-
-const PORT = process.env.PORT || config.get<number>('port')
 
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'public/pages'))
 
 app.use(bodyParser.urlencoded({ extended: true }))
-app.use(express.json())
 app.use('/api', express.static(path.join(__dirname, 'public')))
 
-import db from '../config/db'
-import router from './router'
-import Logger from '../config/logger'
-
+app.use(express.json())
 app.use('/api/', router)
 
-app.listen(PORT, async () => {
-    await db()
+app.listen(PORT, () => {
 
+    connectDB.connect()
     Logger.info(`Server runnning PORT ${PORT}`)
+
 })
