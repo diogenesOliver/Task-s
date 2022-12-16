ed94ec0e019e9966547f37ebea2b80a841fccce2
Diógenes Ribeiro de Oliveira <diogenes.riboliveira@gmail.com>
2022-12-16 11:12:00 -0200

    Fixing bug on authenticate user [Progress..]

--- src/UseCases/AuthenticateUser/AuthenticateUser.ts
+++ src/UseCases/AuthenticateUser/AuthenticateUser.ts
@@ -1,7 +1,15 @@
 import { Request, Response } from 'express'
 import { UserModel } from "../../model/User";
 
+import * as bcrypt from 'bcrypt'
+
 import Logger from '../../../config/logger'
+
+interface IPasswordInterface{
+
+    password: string | undefined
+
+}
 
 export class AuthenticateUser {
 
@@ -10,6 +18,9 @@ export class AuthenticateUser {
         const { email, password } = req.body
 
         const user = await UserModel.findOne({ email }).select('+password')
+        const verifyPassword = bcrypt.compareSync(password, user.password)
+
+        console.log(verifyPassword)
 
         if (!user)
             return res.status(404).send({ error: 'User não encontrado!' })
