d579d76f2bc103f14617b804791c160d61f053d6
Diógenes Ribeiro de Oliveira <diogenes.riboliveira@gmail.com>
2022-12-19 11:56:52 -0200

    Remocing commented code

--- src/UseCases/CreateTask/CreateTaskController.ts
+++ src/UseCases/CreateTask/CreateTaskController.ts
@@ -19,6 +19,8 @@ export class CreateTaskController{
                 difficulty
             })
 
+            /* return res.status(200).redirect('/api/home') */
+
         }catch(e: any){
 
             return res.status(500).json({
@@ -30,23 +32,3 @@ export class CreateTaskController{
     }
 
 }
-
-/* export class CreateTask{
-
-    async createNewTask(req: Request, res: Response){
-
-        try{
-
-            const data = req.body
-            const task = await TaskModel.create(data)
-    
-            return res.status(200).redirect('/api/home')
-    
-        }catch(e: any){
-            Logger.error(`Error on System: ${e.message}`)
-            return res.status(500).json({ error: 'Houve um erro tente novamente mais tarde!' })
-        }
-
-    }
-
-} */
