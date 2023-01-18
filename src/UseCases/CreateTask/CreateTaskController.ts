0cadcd1001fd51d3584ff2d80a330d2bc0a7e48e
Diógenes Ribeiro de Oliveira <diogenes.riboliveira@gmail.com>
2023-01-18 20:49:47 -0200

    feat: Applying SOLID to Create a new Task feature

--- src/UseCases/CreateTask/CreateTaskController.ts
+++ src/UseCases/CreateTask/CreateTaskController.ts
@@ -8,27 +8,8 @@ export class CreateTaskController{
     ){}
 
     async handle(req: Request, res: Response){
-
-        const { title, description, difficulty } = req.body
-
-        try{
-
-            await this.createTaskUseCase.execute({
-                title,
-                description,
-                difficulty
-            })
-
-            /* return res.status(200).redirect('/api/home') */
-
-        }catch(e: any){
-
-            return res.status(500).json({
-                message: e.message || 'Unxpected error'
-            })
-
-        }
-
+        const tasksData = req.body
+        await this.createTaskUseCase.execute(tasksData)
     }
 
 }
