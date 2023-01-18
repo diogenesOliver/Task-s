e59c337eef96de395507251abec00acb25310725
Diógenes Ribeiro de Oliveira <diogenes.riboliveira@gmail.com>
2023-01-18 20:48:35 -0200

    feat: Adding dependency injection to create a new task feature

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -3,7 +3,15 @@ import { container } from "tsyringe";
 import { CreateUserRepository } from "../../repositories/implementations/CreateUserRepository";
 import { ICreateUserRepository } from "../../repositories/ICreateUserRepository";
 
+import { CreateTaskRepository } from '../../repositories/implementations/CreateTasksRepository'
+import { ICreateTaskRepository } from '../../repositories/ICreateTaskRepository'
+
 container.registerSingleton<ICreateUserRepository>(
     "CreateUserRepository",
     CreateUserRepository
+)
+
+container.registerSingleton<ICreateTaskRepository>(
+    "CreateTaskRepository",
+    CreateTaskRepository
 )
