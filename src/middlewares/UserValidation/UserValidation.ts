0a52df803203b28950664b69a4e0606262079e13
Diógenes Ribeiro de Oliveira <diogenes.riboliveira@gmail.com>
2023-02-03 10:40:05 -0200

    feat: POO to createUserValidation feature

---
+++ src/middlewares/UserValidation/UserValidation.ts
@@ -0,0 +1,25 @@
+import { body } from "express-validator";
+
+export class CreateUserValidation{
+
+    dataValidations(){
+
+        return [
+
+            body('email')
+                .isString()
+                .isEmail()
+                .withMessage('O email inválido')
+                .isLength({ min: 20 })
+                .withMessage('O email informado é curto de mais'),
+    
+            body('password')
+                .isString()
+                .withMessage('A senha deve ter no mínimo dois caracteres especiais')
+                .isLength({ min: 6 })
+                .withMessage('A senha informada é curta de mais')
+        ]
+
+    }
+
+}
---
+++ src/middlewares/UserValidation/userValidationIndex.ts
@@ -0,0 +1,5 @@
+import { CreateUserValidation } from "./UserValidation";
+
+const createUserValidate = new CreateUserValidation()
+
+export { createUserValidate }
