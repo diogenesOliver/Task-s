1157db475f9315c6e78e03a160add959bbb287a0
Diógenes Ribeiro de Oliveira <diogenes.riboliveira@gmail.com>
2023-02-06 12:01:35 -0200

    fix: Removing  select false by UserSchema Model

--- src/model/User.ts
+++ src/model/User.ts
@@ -9,8 +9,7 @@ const userSchema = new Schema(
         },
         password: {
             type: String,
-            require: true,
-            select: false
+            require: true
         }
     },
     {
