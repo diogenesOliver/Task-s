e9c9258ffb5739d3c166668acf780a39e603b981
Diógenes Ribeiro de Oliveira <diogenes.riboliveira@gmail.com>
2023-04-03 10:30:43 -0300

    Wip: Working in JWT middleware

--- src/middlewares/JWTAuthorization/JWTHandleMiddleware.ts
+++ src/middlewares/JWTAuthorization/JWTHandleMiddleware.ts
@@ -6,15 +6,25 @@ class JsonWebTokenHandleMiddleware {
     verifyJWT(req: Request, res: Response, next: NextFunction) {
         const authHeader = req.headers['x-access-token'] as string
         const token = authHeader && authHeader.split(" ")[1]
+        
+        /* const SECRET_KEY = process.env.SECRET as string
 
-        if(!token)
+        verify(authHeader, SECRET_KEY, (err: any, decode: any) => {
+            if (err)
+                res.status(403).end()
+
+            decode = req.body
+            res.json(decode)
+        }) */
+
+        if (!token)
             return res.status(401).json({ msg: "Acesso negado!" })
 
-        try{
+        try {
             const SECRET = process.env.SECRET
             verify(token, SECRET as string)
             next()
-        }catch(err){
+        } catch (err) {
             res.status(401).json({
                 msg: 'TOKEN invalid'
             })
