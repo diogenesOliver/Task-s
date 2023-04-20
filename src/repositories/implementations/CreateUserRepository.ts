350c4f08e7cdc66f47fdb0b7afe8b7b1339c1b43
Diógenes Ribeiro de Oliveira <91908375+diogenesOliver@users.noreply.github.com>
2023-04-20 00:49:00 +0000

    feat: SQL - Query to create  a  new user

--- src/repositories/implementations/CreateUserRepository.ts
+++ src/repositories/implementations/CreateUserRepository.ts
@@ -2,11 +2,13 @@ import { UserEntitie } from "../../entities/User";
 import { ICreateUserRepository } from "../ICreateUserRepository";
 import { IUserModel, User } from "../../model/User";
 
+import { PrismaClient } from "@prisma/client";
+
 export class CreateUserRepository implements ICreateUserRepository{
-
-    async save(user: IUserModel): Promise<IUserModel> {
-        await User.create(user)
-        return user
-    }
-
+    async save(user: any): Promise<IUserModel> {
+        const prismClient = new PrismaClient()
+        return await prismClient.user.create({
+            data: user
+        })
+    }    
 }
