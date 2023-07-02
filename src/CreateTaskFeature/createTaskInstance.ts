a7b16cdd681765f7a2df57152f15aa66db5a76a9
diogenesOliver <diogenes.riboliveira@gmail.com>
2023-07-02 14:02:25 -0300

    feat: Feature CreateTask Created

---
+++ src/CreateTaskFeature/createTaskInstance.ts
@@ -0,0 +1,6 @@
+import { CreateTaskController } from "./CreateTaskController";
+import { CreateTaskService } from "../repositories/CreateTaskService";
+
+export const createTaskInstance = new CreateTaskController(
+    new CreateTaskService()
+)
