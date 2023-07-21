fd1fe00ce10d14dd9fb8b57253ba48719a1717e0
diogenesOliver <diogenes.riboliveira@gmail.com>
2023-07-21 15:22:07 -0300

    feat: Initialize components

---
+++ web/src/components/ButtonComponent.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+defineProps<{
+    msg: string
+}>()
+</script>
+
+<template>
+    <button>{{ msg }}</button>
+</template>
+
+<style scoped>
+button {
+    outline: none;
+    border: none;
+    cursor: pointer;
+
+    background-color: #00bd7e;
+    color: white;
+
+    height: 2rem;
+    width: 10rem;
+
+    border-radius: .3rem;
+    margin-top: 1rem;
+
+    transition: .3s;
+}
+
+button:hover{
+    background-color: #018d5e;
+}
+</style>
---
+++ web/src/components/HelloWorld.vue
@@ -0,0 +1,43 @@
+<script setup lang="ts">
+import ButtonComponent from './ButtonComponent.vue';
+
+defineProps<{
+  msg: string
+}>()
+</script>
+
+<template>
+  <div class="greetings">
+    <h1 class="green">{{ msg }}</h1>
+    <h3>
+      Task-s! is an easy and intuitive tool <br>
+      sou you can have control over all you duties
+    </h3>
+    <ButtonComponent msg="Get Started"/>
+  </div>
+</template>
+
+<style scoped>
+h1 {
+  font-weight: 500;
+  font-size: 2.6rem;
+  position: relative;
+  top: -10px;
+}
+
+h3 {
+  font-size: 1.2rem;
+}
+
+.greetings h1,
+.greetings h3 {
+  text-align: center;
+}
+
+@media (min-width: 1024px) {
+  .greetings h1,
+  .greetings h3 {
+    text-align: left;
+  }
+}
+</style>
