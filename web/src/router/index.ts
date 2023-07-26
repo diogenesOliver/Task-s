ea19b48c13db6593c104b5cc0946f85a4ec91bbe
diogenesOliver <diogenes.riboliveira@gmail.com>
2023-07-26 15:34:29 -0300

    feat: creating routes from viwers

--- web/src/router/index.ts
+++ web/src/router/index.ts
@@ -1,18 +1,20 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+
+import HomeView from '../views/SignInView.vue'
+import AboutView from '../views/SignUpView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
+      path: '/sign-in',
       name: 'home',
       component: HomeView
     },
     {
-      path: '/about',
+      path: '/login',
       name: 'about',
-      component: () => import('../views/AboutView.vue')
+      component: AboutView
     }
   ]
 })
