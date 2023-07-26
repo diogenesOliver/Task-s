fe0da1e982e903c77fa1271358c1860bb1d89429
diogenesOliver <diogenes.riboliveira@gmail.com>
2023-07-26 17:08:20 -0300

    feat: Changing file name

--- web/src/views/SignUpView.vue
+++ web/src/views/SignUpView.vue
@@ -1,11 +1,11 @@
 <script setup lang="ts">
 import TheWelcome from '../components/TheWelcome.vue'
-import SignInComponent from '../components/SignInComponent.vue'
+import SignUpComponent from '../components/SignUpComponent.vue'
 </script>
 
 <template>
   <main>
     <TheWelcome />
-    <SignInComponent/>
+    <SignUpComponent/>
   </main>
 </template>
