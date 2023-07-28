8146d78f3b39061558fb7571d0c19078926a6a93
diogenesOliver <diogenes.riboliveira@gmail.com>
2023-07-28 14:59:47 -0300

    feat: removing one empty component file

--- web/src/views/SignUpView.vue
+++ web/src/views/SignUpView.vue
@@ -1,11 +1,9 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
 import SignUpComponent from '../components/SignUpComponent.vue'
 </script>
 
 <template>
   <main>
-    <TheWelcome />
     <SignUpComponent/>
   </main>
 </template>
