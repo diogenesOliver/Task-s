578a47bcb337ae5f053f0c052497cae8f1e84b4a
diogenesOliver <diogenes.riboliveira@gmail.com>
2023-07-30 18:04:47 -0300

    feat: Verify if password and confirm_password does match

--- src/CreateUserFeature/CreateUserController.ts
+++ src/CreateUserFeature/CreateUserController.ts
@@ -2,17 +2,20 @@ import { Request, Response } from "express";
 import { CreateUserService } from '../repositories/CreateUserService'
 import { User } from "@prisma/client";
 
-export class CreateUserController{
+export class CreateUserController {
     constructor(
         private createUserService: CreateUserService
-    ){}
+    ) { }
 
-    async createUserController(req: Request, res: Response){
-        try{
+    async createUserController(req: Request, res: Response) {
+        try {
             const userData: User = req.body
+            if(userData.password != userData.confirm_password)
+                return res.status(404).send('Passwor not match')
+
             this.createUserService.save(userData)
 
             res.status(200).send('Success to create user')
-            }catch(e){ console.log(e) }
+        } catch (e) { console.log(e) }
     }
 }
