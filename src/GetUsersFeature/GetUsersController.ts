7af3ee5f2e68380ade0499a9b7892bb586188649
diogenesOliver <diogenes.riboliveira@gmail.com>
2023-07-30 18:06:00 -0300

    feat: adding try catch from not stop server on ERROR

--- src/GetUsersFeature/GetUsersController.ts
+++ src/GetUsersFeature/GetUsersController.ts
@@ -1,18 +1,20 @@
 import { Request, Response } from "express";
 import { GetUserService } from "../repositories/GetUsersService";
 
-export class GetUserController{
+export class GetUserController {
     constructor(
         private getUserService: GetUserService
-    ){}
+    ) { }
 
-    async getUserController(req: Request, res: Response){
-        const allUsers = await this.getUserService.get()
-        res.status(200).send(allUsers)
-    
-        return{
-            statusCode: 200,
-            body: allUsers
-        }
+    async getUserController(req: Request, res: Response) {
+        try {
+            const allUsers = await this.getUserService.get()
+            res.status(200).send(allUsers)
+
+            return {
+                statusCode: 200,
+                body: allUsers
+            }
+        }catch(e){ console.log(e) }
     }
 }
