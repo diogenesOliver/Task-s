adf85f0d6e01febafa52d1be2efa171f9df94ea8
diogenesOliver <diogenes.riboliveira@gmail.com>
2023-08-09 22:13:46 -0300

    feat: UserLogin Feature created

--- web/src/components/SignUpComponent.vue
+++ web/src/components/SignUpComponent.vue
@@ -1,7 +1,3 @@
-<script setup lang="ts">
-import ButtonComponent from './ButtonComponent.vue';
-</script>
-
 <template>
   <h1>Login on plataform</h1>
   <p>Keep managing and organizing your tasks in the best possible way.<br> Enter the platform and see how your tasks are
@@ -9,11 +5,11 @@ import ButtonComponent from './ButtonComponent.vue';
 
   <div class="login">
     <form>
-      <input type="email" class="form-input" placeholder="Email">
-      <input type="password" class="form-input" placeholder="Password">
+      <input type="email" class="form-input" placeholder="Email" v-model="userData.email">
+      <input type="password" class="form-input" placeholder="Password" v-model="userData.password">
     </form>
 
-    <ButtonComponent msg="Login" class="login-button" />
+    <button class="login-button" @click.prevent="userLogin()">Login</button>
     <small>Or Login In Using</small>
     <button class="login-with-google">
       <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/google/google-original.svg" />
@@ -25,6 +21,31 @@ import ButtonComponent from './ButtonComponent.vue';
     </router-link>
   </div>
 </template>
+
+<script lang="ts">
+import axios from 'axios'
+
+export default{
+  name: "Login",
+  data(){
+    return{
+      userData: {
+        email: "",
+        password: ""
+      }
+    }
+  },
+  methods: {
+    async userLogin(){
+      try{
+        await axios.post("http://localhost:8080/api/user/sign-up", this.userData).then(res => {
+          console.log(res.data)
+        })
+      }catch(e){ console.log(e) }
+    }
+  }
+}
+</script>
 
 <style>
 .login {
@@ -93,6 +114,18 @@ import ButtonComponent from './ButtonComponent.vue';
   border: none;
 
   cursor: pointer;
+}
+
+.login-button {
+    outline: none;
+    border: none;
+    cursor: pointer;
+
+    background: linear-gradient(90deg, hsl(276, 51%, 47%) 0%, rgba(253,29,29,1) 65%, rgba(252,176,69,1) 100%);
+    color: white;
+
+    border-radius: .5rem;
+    transition: .3s;
 }
 
 .login-with-google img {
