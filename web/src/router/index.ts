172d0c06e2da76b22785bf3b1967f8afa6728bdd
diogenesOliver <diogenes.riboliveira@gmail.com>
2023-08-17 09:30:33 -0300

    feat: new rout created

--- web/src/router/index.ts
+++ web/src/router/index.ts
@@ -3,6 +3,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/SignInView.vue'
 import AboutView from '../views/SignUpView.vue'
 import HomePageView from '../views/HomePageView.vue'
+import TaskPageView from '../views/TaskPageView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -21,6 +22,11 @@ const router = createRouter({
       path: '/login',
       name: 'about',
       component: AboutView
+    },
+    {
+      path: '/task-s/:name/:id',
+      name: "tasks",
+      component: TaskPageView
     }
   ]
 })
