35cb27077812790cb9abdb67ff9fb6027227908b
diogenesOliver <diogenes.riboliveira@gmail.com>
2023-08-18 16:47:42 -0300

    feat: Modal created

--- web/src/components/TaskPageComponent.vue
+++ web/src/components/TaskPageComponent.vue
@@ -2,13 +2,48 @@
     <div class="wrapper">
         <div class="nav-div">
             <div class="infoir">
-                <h1>These are your Task-s! {{ userInfo.name }}</h1>
+                <h1>Welcome to the Task-s, {{ userInfo.name }}!</h1>
             </div>
-            <button class="new-task">New Task</button>
+            <button class="new-task" @click.prevent="openModal()">New Task</button>
+        </div>
+
+        <div class="modal-wrapper" id="modalWrapper">
+            <div class="modal" id="modal">
+                <div class="icon-close-modal">
+
+                    <h3>New Task</h3>
+
+                    <svg @click.prevent="closeModal()" width="40" height="40" viewBox="0 0 40 40" fill="none"
+                        xmlns="http://www.w3.org/2000/svg">
+                        <path d="M30 10L10 30M10 10L30 30" stroke="grey" stroke-width="2" stroke-linecap="round"
+                            stroke-linejoin="round" />
+                    </svg>
+                </div>
+
+                <div class="task-input">
+                    <form>
+                        <input type="email" class="form-input" placeholder="Title">
+                        <input type="email" class="form-input" placeholder="Description">
+                        <div class="forms">
+                            <input type="email" class="form-input" placeholder="Difficulty">
+                            <input type="email" class="form-input" placeholder="Difficulty">
+                        </div>
+                    </form>
+                </div>
+
+                <div class="buttons">
+                    <button class="create-task">Create</button>
+                    <button class="cancel-task" @click="closeModal()">Cancel</button>
+                </div>
+            </div>
         </div>
 
         <div class="msg-from-0-tasks" id="taskCounter">
             <h1>You don't have any tasks created. <br> Create a new task and view it on this screen</h1>
+        </div>
+
+        <div class="tasks-card">
+            <p>{{ userInfo.Task }}</p>
         </div>
 
     </div>
@@ -41,6 +76,20 @@ export default {
                     taskCounter.style.display = 'none'
                 }
             })
+        },
+        async openModal() {
+            const modalDiv = document.getElementById('modalWrapper') as HTMLElement
+            const modal = document.getElementById('modal') as HTMLElement
+
+            modalDiv.style.display = 'flex'
+            modal.style.display = 'flex'
+        },
+        async closeModal() {
+            const modalDiv = document.getElementById('modalWrapper') as HTMLElement
+            const modal = document.getElementById('modal') as HTMLElement
+
+            modalDiv.style.display = 'none'
+            modal.style.display = 'none'
         }
     },
     mounted() {
@@ -70,7 +119,9 @@ h1 {
     font-size: 1.5rem
 }
 
-.new-task {
+.new-task,
+.create-task,
+.cancel-task {
     height: 2.3rem;
     width: 8rem;
 
@@ -89,6 +140,102 @@ h1 {
     text-align: center;
     margin-top: 20rem;
 
-    color: rgba(128, 128, 128, 0.589);
+    color: #80808096;
+}
+
+.modal {
+    display: none;
+    flex-direction: column;
+    align-items: center;
+
+    gap: 3rem;
+    padding: 1rem;
+
+    background-color: #181818;
+
+    border-radius: .2rem;
+    padding-inline: 2rem;
+
+    width: 50rem;
+    height: 29rem;
+
+    margin-inline: auto;
+    margin-top: 10%;
+
+    animation: modal-animation .3s;
+}
+
+.modal-wrapper {
+    background: rgba(0, 0, 0, .5);
+    display: none;
+
+    position: fixed;
+
+    top: 0;
+    left: 0;
+
+    width: 100%;
+    height: 100%;
+
+    z-index: 100;
+}
+
+@keyframes modal-animation {
+    0% {
+        opacity: 0;
+        transform: scale(.2);
+    }
+
+    100% {
+        opacity: 1;
+        transform: scale(1);
+    }
+}
+
+.icon-close-modal,
+.task-input,
+.buttons {
+    width: 95%;
+}
+
+.icon-close-modal {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+
+    align-items: center;
+}
+
+svg {
+    cursor: pointer;
+    height: 2.3rem;
+}
+
+.task-inpput {
+    height: 85%;
+}
+
+.forms {
+    display: flex;
+    flex-direction: row;
+
+    gap: 2rem;
+}
+
+.buttons {
+    display: flex;
+    flex-direction: row-reverse;
+
+    gap: 2rem;
+}
+
+.cancel-task,
+.create-task {
+    width: 10rem;
+}
+
+.cancel-task {
+    background: transparent;
+    border: 1px solid grey;
 }
 </style>
