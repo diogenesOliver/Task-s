888be98adb899ddbed70f64cd98cf7c8d2f335b8
diogenesOliver <diogenes.riboliveira@gmail.com>
2023-09-07 15:00:50 -0300

    feat: Crypting password by create a new user

--- src/CreateUserFeature/CreateUserController.ts
+++ src/CreateUserFeature/CreateUserController.ts
@@ -1,4 +1,6 @@
 import { Request, Response } from "express";
+import { genSalt, hash } from 'bcrypt'
+
 import { CreateUserService } from '../repositories/CreateUserService'
 import { User } from "@prisma/client";
 
@@ -14,15 +16,23 @@ export class CreateUserController {
             type userDataComponents = [string, string, string, string]
             const userDataArray: userDataComponents = [userData.name, userData.email, userData.password, userData.confirm_password]
 
-            for(let data of userDataArray){
-                if(data == '')
+            for (let data of userDataArray) {
+                if (data == '')
                     return res.status(404).send('ERROR')
             }
 
-            if(userData.password != userData.confirm_password)
+            if (userData.password != userData.confirm_password)
                 return res.status(404).send('Passwor not match')
 
-            const createUserExec =  await this.createUserService.save(userData)
+            const SALT: string = await genSalt(14)
+            const CRYPTO_PASSWORD: string = await hash(
+                userData.password, SALT
+            )
+
+            userData.password = CRYPTO_PASSWORD
+            userData.confirm_password = CRYPTO_PASSWORD
+
+            const createUserExec = await this.createUserService.save(userData)
             return res.status(200).send(createUserExec)
         } catch (e) { console.log(e) }
     }
