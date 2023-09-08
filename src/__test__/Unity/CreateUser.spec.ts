658285a4cf00c7591c06816823cc10335947d303
diogenesOliver <diogenes.riboliveira@gmail.com>
2023-09-08 13:07:45 -0300

    feat: Test unity from createUserController created

--- src/__test__/Unity/CreateUser.spec.ts
+++ src/__test__/Unity/CreateUser.spec.ts
@@ -3,10 +3,13 @@ import 'reflect-metadata'
 import { CreateUserService } from '../../repositories/CreateUserService'
 import { User } from '@prisma/client'
 
+const EMAIL_RANDOM: string = Math.random().toString(36).slice(2)
+const ID_RANDOM: number = Math.floor(Math.random() * 100)
+
 const dataFromTest: User = {
-    id: 100,
+    id: ID_RANDOM,
     name: "Diógenes Ribeiro de Oliveira",
-    email: "diogenes.oliveira@gmail.com",
+    email: `${EMAIL_RANDOM}.oliveira@gmail.com`,
     password: "senha123123",
     confirm_password: "senha123123",
     createdAt: new Date(),
@@ -16,8 +19,11 @@ const dataFromTest: User = {
 const createUserInstance = new CreateUserService()
 
 describe('This is a unity test from controller function', () => {
-    it('Testing save on DataBase function', async () => {
+    it('Testing that there are all elements in the response', async () => {
         const createUserExec = await createUserInstance.save(dataFromTest)
-        console.log(createUserExec)
+        const keysArray: string[] = ['id', 'name', 'password', 'confirm_password', 'createdAt', 'updatedAt']
+        keysArray.forEach(element => {
+            expect(createUserExec).toHaveProperty(element)
+        });
     })
 })
