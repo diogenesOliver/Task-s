f7d87567a9d7e2d544ff6a99596abd4f38d33d9c
diogenesOliver <diogenes.riboliveira@gmail.com>
2023-09-10 11:54:18 -0300

    style: Changin title

--- src/__test__/Unity/CreateTask.spec.ts
+++ src/__test__/Unity/CreateTask.spec.ts
@@ -20,7 +20,7 @@ let tasksFromTests: Task = {
 
 const createTaskServiceInstance = new CreateTaskService()
 
-describe('This is a unity test from createTaskController function', () => {
+describe('This is a unity test from createTaskService function', () => {
     it('Testing that there are all elements in the response', async () => {
         const createTaskExec = await createTaskServiceInstance.save(tasksFromTests)
         const keysArray: string[] = ['id', 'title', 'description', 'difficulty', 'status', 'authorId', 'createdAt', 'updatedAt']
