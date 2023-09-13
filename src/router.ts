da1cf934fa40dd859200e256374ebb9d87979a50
diogenesOliver <diogenes.riboliveira@gmail.com>
2023-09-13 14:27:01 -0300

    feat: Adding all routers on ARRAY

--- src/router.ts
+++ src/router.ts
@@ -1,32 +1,33 @@
 import { Router } from 'express'
-
 import { Response, Request } from 'express-serve-static-core'
 
 const router = Router()
 
+import { getUserInstance } from "./GetUsersFeature/getUsersInstance"
 import { createUserInstance } from "./CreateUserFeature/createUserInstance"
 import { createTaskInstance } from "./CreateTaskFeature/createTaskInstance"
-import { getUserInstance } from "./GetUsersFeature/getUsersInstance"
 import { userLoginInstance } from './LoginUserFeature/loginUserInstance'
 import { getTaskInstance } from './GetTasksFeature/getTaskInstance'
 
-router.get('/all/users/:id', (req: Request, res: Response) => {
+const urlArrays: string[] = ['/all/users/:id', '/register', '/create/task', '/user/sign-up', '/all/tasks']
+
+router.get(urlArrays[0], (req: Request, res: Response) => {
     return getUserInstance.getUserController(req, res)
 })
 
-router.post('/register', (req: Request, res: Response) => {
+router.post(urlArrays[1], (req: Request, res: Response) => {
     return createUserInstance.createUserController(req, res)
 })
 
-router.post('/create/task', (req: Request, res: Response) => {
+router.post(urlArrays[2], (req: Request, res: Response) => {
     return createTaskInstance.createTaskController(req, res)
 })
 
-router.post('/user/sign-up', (req: Request, res: Response) => {
+router.post(urlArrays[3], (req: Request, res: Response) => {
     return userLoginInstance.virifyEmailInDatabase(req, res)
 })
 
-router.get('/all/tasks', (req: Request, res: Response) => {
+router.get(urlArrays[4], (req: Request, res: Response) => {
     return getTaskInstance.getTaskController(req, res)
 })
 
