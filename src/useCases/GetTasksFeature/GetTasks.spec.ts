3798eadfc3450386e28e2502ed4d90958b009fa5
diogenesOliver <diogenes.riboliveira@gmail.com>
2023-09-18 09:34:12 -0300

    feat: Creating a new test from GetAllTasksFeature

---
+++ src/useCases/GetTasksFeature/GetTasks.spec.ts
@@ -0,0 +1,12 @@
+import { test, expect } from "vitest";
+import { GetTasksService } from "../../repositories/GetTasksService";
+
+test('Get all tasks', async () => {
+    const getTasks = await new GetTasksService().returninAll()
+
+    if(getTasks.length > 0){
+        expect(getTasks[0]).toHaveProperty('title')
+    }
+
+    expect(getTasks).toBeTypeOf('object')
+})
