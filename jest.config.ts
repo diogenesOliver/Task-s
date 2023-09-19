caf7b12619a7f1fc590f896dad243a10c13ea45c
diogenesOliver <diogenes.riboliveira@gmail.com>
2023-09-19 11:46:13 -0300

    fix: Fixing identation

--- jest.config.ts
+++ jest.config.ts
@@ -1,11 +1,11 @@
 export default {
-  preset: 'ts-jest',
-  automock: false,
-  rootDir: '.',
-  collectCoverage: true,
-  coverageDirectory: 'coverage',
-  coverageProvider: 'v8',
-  testMatch: [
-    '<rootDir>/**/*.spec.ts',
-  ],
-};
+	preset: 'ts-jest',
+	automock: false,
+	rootDir: '.',
+	collectCoverage: true,
+	coverageDirectory: 'coverage',
+	coverageProvider: 'v8',
+	testMatch: [
+		'<rootDir>/**/*.spec.ts',
+	],
+}
