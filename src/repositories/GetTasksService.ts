b17a71cf094204712dd67b14c3b7b507d7fde21f
diogenesOliver <diogenes.riboliveira@gmail.com>
2023-09-28 15:21:23 -0300

    feat: Adding type from functions

--- src/repositories/GetTasksService.ts
+++ src/repositories/GetTasksService.ts
@@ -1,7 +1,7 @@
 import { IGetAll, PrismaClientInstance } from './GenericsInterfaces/GenericRepository'
 
-export class GetTasksService implements IGetAll<any>{
-	returninAll(): Promise<any> {
-		return PrismaClientInstance.task.findMany()
+export class GetTasksService implements IGetAll<object[]>{
+	async returninAll(): Promise<object[]> {
+		return await PrismaClientInstance.task.findMany() as object[]
 	}
 }
