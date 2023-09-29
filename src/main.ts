15849d381bfa0ba6370cf5ed959aa52ebef24379
diogenesOliver <diogenes.riboliveira@gmail.com>
2023-09-29 14:27:10 -0300

    style: Changing importing of the URLENcoded

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,12 @@ import 'reflect-metadata'
 import express from 'express'
 import router from './router'
 
-import * as bodyParser from 'body-parser'
+import { urlencoded } from 'body-parser'
 import cors from 'cors'
 
 const app = express()
 
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(urlencoded({ extended: true }))
 
 app.use(cors())
 app.use(express.json())
