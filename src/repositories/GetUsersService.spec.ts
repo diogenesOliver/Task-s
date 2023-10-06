6572225b3cb4093c349ae47471aa30fa52aa570b
diogenesOliver <diogenes.riboliveira@gmail.com>
2023-10-06 13:07:42 -0300

    feat: Adding user ID on a variable

--- src/repositories/GetUsersService.spec.ts
+++ src/repositories/GetUsersService.spec.ts
@@ -2,15 +2,16 @@ import { expect, describe, it, test } from 'vitest'
 import { GetUserService } from './GetUsersService'
 
 const findUserById = new GetUserService()
+const userIdExemple: number = 3 
 
 test('Get one user by id and verify typeof', async () => {
 	expect(
-		await findUserById.get(57)
+		await findUserById.get(userIdExemple)
 	).toBeTypeOf('object')
 })
 
 test('Verifying if have property name', async () => {
 	expect(
-		await findUserById.get(57)
+		await findUserById.get(userIdExemple)
 	).toHaveProperty('name')
 })
