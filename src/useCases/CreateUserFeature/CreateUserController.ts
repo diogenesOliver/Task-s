7494fc08dfc98c6180976eea14f4f942fe5e188b
diogenesOliver <diogenes.riboliveira@gmail.com>
2023-10-25 14:55:48 -0300

    feat: Function from cryptPassword created

--- src/useCases/CreateUserFeature/CreateUserController.ts
+++ src/useCases/CreateUserFeature/CreateUserController.ts
@@ -9,10 +9,24 @@ export class CreateUserController {
 		private createUserService: CreateUserService
 	) { }
 
+	private async cryptingPassword(password: string, confirmPassword: string) {
+		const SALT: string = await genSalt(14)
+		const CRYPTO_PASSWORD: string = await hash(
+			password, SALT
+		)
+
+		password = CRYPTO_PASSWORD
+		confirmPassword = CRYPTO_PASSWORD
+
+		return {
+			password,
+			confirmPassword
+		}
+	}
+
 	async createUserController(req: Request, res: Response) {
 		try {
 			const userData: User = req.body
-
 			const userDataArray = [userData.name, userData.email, userData.password, userData.confirm_password]
 
 			for (const data of userDataArray) {
@@ -21,15 +35,12 @@ export class CreateUserController {
 			}
 
 			if (userData.password != userData.confirm_password)
-				return res.status(404).send('Passwor not match')
+				return res?.status(404).send('Password not match')
 
-			const SALT: string = await genSalt(14)
-			const CRYPTO_PASSWORD: string = await hash(
-				userData.password, SALT
-			)
+			const cryptingPassword = await this.cryptingPassword(userData.password, userData.confirm_password)
 
-			userData.password = CRYPTO_PASSWORD
-			userData.confirm_password = CRYPTO_PASSWORD
+			userData.password = cryptingPassword.password
+			userData.confirm_password = cryptingPassword.confirmPassword
 
 			const createUserExec = await this.createUserService.save(userData)
 			return res.status(200).send(createUserExec)
