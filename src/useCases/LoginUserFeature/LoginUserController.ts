ab5859da1fa868bc5ac44148f7adc0585595d4ce
diogenesOliver <diogenes.riboliveira@gmail.com>
2023-11-02 12:52:30 -0300

    feat: Creating a TOKEN from authorization

--- src/useCases/LoginUserFeature/LoginUserController.ts
+++ src/useCases/LoginUserFeature/LoginUserController.ts
@@ -1,6 +1,11 @@
+import { config } from 'dotenv'
+config()
+
 import { Request, Response } from 'express'
-import { UserLoginService } from '../../repositories/LoginUserService/LoginUserService'
 import { compare } from 'bcrypt'
+import { sign } from 'jsonwebtoken'
+
+import { UserLoginService } from '../../repositories/LoginUserService/LoginUserService'
 import { User } from '@prisma/client'
 
 export class UserLoginController {
@@ -12,6 +17,12 @@ export class UserLoginController {
 		return await compare(password, cryptPassword)
 	}
 
+	private generateAToken(userId: string): string {
+		return sign({}, process.env.SECRET_KEY as string, {
+			subject: userId
+		})
+	}
+
 	async verifyEmailInDatabase(req: Request, res: Response) {
 		try {
 			const inputData: User = req.body
@@ -21,7 +32,9 @@ export class UserLoginController {
 			if (user == false)
 				return res.status(404).json({ msg: 'Some Error' })
 
-			return res.status(200).send(findEmail)
+			const token = this.generateAToken(findEmail.id.toString())
+
+			return res.status(200).json(token)
 		} catch (e) { console.log(e) }
 	}
 }
