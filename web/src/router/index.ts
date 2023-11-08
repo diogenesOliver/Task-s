01dece4956f65f67dda12ba1f97da359c0be4861
diogenesOliver <diogenes.riboliveira@gmail.com>
2023-11-08 00:30:59 -0300

    style: Identation

--- web/src/router/index.ts
+++ web/src/router/index.ts
@@ -6,29 +6,29 @@ import HomePageView from '../views/HomePageView.vue'
 import TaskPageView from '../views/TaskPageView.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'homePage',
-      component: HomePageView
-    },
-    {
-      path: '/sign-in',
-      name: 'home',
-      component: HomeView
-    },
-    {
-      path: '/login',
-      name: 'about',
-      component: AboutView
-    },
-    {
-      path: '/task-s/:name/:id',
-      name: "tasks",
-      component: TaskPageView
-    }
-  ]
+	history: createWebHistory(import.meta.env.BASE_URL),
+	routes: [
+		{
+			path: '/',
+			name: 'homePage',
+			component: HomePageView
+		},
+		{
+			path: '/sign-in',
+			name: 'home',
+			component: HomeView
+		},
+		{
+			path: '/login',
+			name: 'about',
+			component: AboutView
+		},
+		{
+			path: '/task-s/:name/:id',
+			name: 'tasks',
+			component: TaskPageView
+		}
+	]
 })
 
 export default router
