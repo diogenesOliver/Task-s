23350102f63756cd3b22f0906768bc81d2c52b81
diogenesOliver <diogenes.riboliveira@gmail.com>
2023-11-07 22:39:27 -0300

    style: done.

--- src/useCases/LoginUserFeature/loginUserInstance.ts
+++ src/useCases/LoginUserFeature/loginUserInstance.ts
@@ -2,5 +2,5 @@ import { UserLoginController } from './LoginUserController'
 import { UserLoginService } from '../../repositories/LoginUserService/LoginUserService'
 
 export const userLoginInstance = new UserLoginController(
-	new UserLoginService
+	new UserLoginService()
 )
