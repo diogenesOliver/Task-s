2b835b110c45a0fdc15c1ac9c66811d9f0ac304a
diogenesOliver <diogenes.riboliveira@gmail.com>
2023-12-14 11:05:20 -0300

    feat: getting if error == true by initializing redis client

--- src/server.ts
+++ src/server.ts
@@ -2,13 +2,15 @@ import { createClient } from 'redis'
 import { config } from 'dotenv'
 config()
 
-export const client = createClient()
-
 import { app } from './main'
 const PORT = process.env.SERVER_PORT || 3001
 
 const startup = async () => {
-	await client.connect()
+	
+	await createClient().on('error', err => {
+		console.log('Redis Client Error', err)
+	}).connect()
+
 	app.listen(PORT, async () => {
 		console.log(`Server running on port: ${PORT}`)
 	})
