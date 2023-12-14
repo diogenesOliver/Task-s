80a755e3d3bedfdda7e4bdfff11b64826c2ddcc0
diogenesOliver <diogenes.riboliveira@gmail.com>
2023-12-14 10:54:04 -0300

    feat: Initializing redis clinte with application server

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,16 @@
+import { createClient } from 'redis'
 import { config } from 'dotenv'
 config()
+
+export const client = createClient()
 
 import { app } from './main'
 const PORT = process.env.SERVER_PORT || 3001
 
-app.listen(PORT, async () => {
-	console.log(`Server running on port: ${PORT}`)
-})
+const startup = async () => {
+	await client.connect()
+	app.listen(PORT, async () => {
+		console.log(`Server running on port: ${PORT}`)
+	})
+}
+startup()
