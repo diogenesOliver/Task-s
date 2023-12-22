09c68c9ee747d798042cdf0bd101f3e44deda99f
diogenesOliver <diogenes.riboliveira@gmail.com>
2023-12-22 18:15:03 -0300

    feat: GetUser feature cached

--- src/useCases/GetUsersFeature/GetUsersController.ts
+++ src/useCases/GetUsersFeature/GetUsersController.ts
@@ -1,6 +1,8 @@
 import { Request, Response } from 'express'
 import { GetUserService } from '../../repositories/GetUserService/GetUsersService'
 
+import { getRedis, redisClient, setRedis } from '../../redisConfig'
+
 export class GetUserController {
 	constructor(
 		private getUserService: GetUserService
@@ -8,8 +10,17 @@ export class GetUserController {
 
 	async getUserController(req: Request, res: Response) {
 		try {
+			const userFromCache = await getRedis('user')
+			if (userFromCache){
+				res.send(JSON.parse(userFromCache))
+				return redisClient.del('user', err => {
+					if(err) throw Error()
+				})
+			}
+
 			const userID = Number(req.params.id)
 			const user = await this.getUserService.get(userID)
+			await setRedis('user', JSON.stringify(user))
 
 			return res.status(200).send(user)
 		} catch (e) { console.log(e) }
