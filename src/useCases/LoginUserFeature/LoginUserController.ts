e1f72e29ec0b83ec9a7f3b6ebfb11f1feb83729f
diogenesOliver <diogenes.riboliveira@gmail.com>
2023-12-26 15:53:24 -0300

    feat: Adding cache on user login  feature

--- src/useCases/LoginUserFeature/LoginUserController.ts
+++ src/useCases/LoginUserFeature/LoginUserController.ts
@@ -4,7 +4,7 @@ config()
 import { Request, Response } from 'express'
 import { compare } from 'bcrypt'
 import { sign, verify } from 'jsonwebtoken'
-
+import { getRedis, redisClient, setRedis } from '../../redisConfig'
 import { UserLoginService } from '../../repositories/LoginUserService/LoginUserService'
 import { User } from '@prisma/client'
 
@@ -26,7 +26,16 @@ export class UserLoginController {
 	async verifyEmailInDatabase(req: Request, res: Response) {
 		try {
 			const inputData: User = req.body
+			const userFromCache = await getRedis('userLogin')
+			if(userFromCache){
+				res.status(200).send(JSON.parse(userFromCache))
+				return redisClient.del('userLogin', err => {
+					if(err) throw Error()
+				})
+			}
+
 			const findEmail = await this.userLoginService.findData(inputData.email)
+			await setRedis('userLogin', JSON.stringify(findEmail))
 
 			const user = await this.verifyPasswordWithCryptPassword(inputData.password, findEmail.password)
 			if (user == false)
