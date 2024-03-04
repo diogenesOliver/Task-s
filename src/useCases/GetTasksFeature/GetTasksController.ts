dd556187e859b4603066569e878d41a5e12b9b27
diogenesOliver <diogenes.riboliveira@gmail.com>
2024-03-04 17:35:21 -0300

    feat: setting time to expire a cache

--- src/useCases/GetTasksFeature/GetTasksController.ts
+++ src/useCases/GetTasksFeature/GetTasksController.ts
@@ -1,7 +1,7 @@
 import { StatusCodes } from '../../logs/statusCode'
 import { FastifyInstance } from 'fastify'
 
-/* import { getRedis, setRedis, redisClient } from '../../redisConfig' */
+import { getRedis, setRedis, redisClient } from '../../redisConfig'
 import { GetTasksService } from '../../repositories/GetTaskService/GetTasksService'
 
 export class GetTaskController {
@@ -12,17 +12,13 @@ export class GetTaskController {
 	async getTaskController(app: FastifyInstance) {
 		app.get('/all/tasks', async (request, reply) => {
 			try {
-				/* const tasksFromCache = await getRedis('tasks')
-				if (tasksFromCache) {
-					reply.send(JSON.parse(tasksFromCache))
-					return redisClient.del('tasks', (err) => {
-						if (err) throw Error()
-					})
-				} */
+				const tasksFromCache = await getRedis('tasks')
+				if (tasksFromCache)
+					return reply.send(JSON.parse(tasksFromCache))
 
 				const tasks = await new GetTasksService().returninAll()
-				/* await setRedis('tasks', JSON.stringify(tasks)) */
-
+				await redisClient.set('tasks', JSON.stringify(tasks), 'EX', 10)
+				
 				return reply.status(StatusCodes.Success).send(tasks)
 			} catch (e) {
 				console.error(e)
