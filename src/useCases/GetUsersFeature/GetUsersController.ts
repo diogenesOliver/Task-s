2a1546ab0581d20c1a43561ce397d01ffd1e4865
diogenesOliver <diogenes.riboliveira@gmail.com>
2024-03-04 17:24:55 -0300

    feat: setting a time to expire cache

--- src/useCases/GetUsersFeature/GetUsersController.ts
+++ src/useCases/GetUsersFeature/GetUsersController.ts
@@ -3,19 +3,16 @@ import { FastifyInstance } from 'fastify'
 import { z } from 'zod'
 
 import { GetUserService } from '../../repositories/GetUserService/GetUsersService'
-import { getRedis, redisClient, setRedis } from '../../redisConfig'
+import { redisClient } from '../../redisConfig'
 
 export class GetUserController {
 	constructor(
 		getUserService: GetUserService
-	) {}
+	) { }
 
 	async getUserController(app: FastifyInstance) {
 		app.get('/user/:id', async (request, reply) => {
 			try {
-				const userFromCache = await getRedis('user')
-				if (userFromCache)
-					return reply.status(StatusCodes.Success).send(JSON.parse(userFromCache))
 
 				const getUserParam = z.object({
 					id: z.string().cuid()
@@ -23,8 +20,12 @@ export class GetUserController {
 
 				const { id } = getUserParam.parse(request.params)
 
+				const userFromCache = await redisClient.get('user')
+				if (userFromCache)
+					return reply.status(StatusCodes.Success).send(JSON.parse(userFromCache))
+
 				const user = await new GetUserService().get(id)
-				await setRedis('user', JSON.stringify(user))
+				await redisClient.set('user', JSON.stringify(user), 'EX', 60)
 
 				return reply.status(StatusCodes.Success).send(user)
 			} catch (e) {
