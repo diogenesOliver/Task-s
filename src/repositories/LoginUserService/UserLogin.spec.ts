13c34c948543b9901cf734309376fe3fdb59245a
diogenesOliver <diogenes.riboliveira@gmail.com>
2024-03-15 20:12:37 -0300

    feat: adding text that should return by erro on find a email and removing unir test from controllers

--- src/repositories/LoginUserService/UserLogin.spec.ts
+++ src/repositories/LoginUserService/UserLogin.spec.ts
@@ -4,17 +4,17 @@ import { describe } from 'node:test'
 
 const userData = new UserLoginService()
 
-describe('Testing the RepositoryClass for user login functionality', () => {
+describe('Testing the RepositoryClass for user login functionality',() => {
 	test('Validating whether the instance is of type Object and returns an object with user data', async () => {
-		const userLogginIn = await userData.findData('diogenes.riboliveira@gmail.com')
-	
+		const userLogginIn = await userData.findData('oliveira.riboliveira@gmail.com')
+
 		expect(userLogginIn).toBeInstanceOf(Object)
 		expect(userLogginIn).toHaveProperty('name')
 	})
-	
+
 	test('Should return an error if the users email is not found', () => {
 		return expect(async () => {
 			await new UserLoginService().findData('')
-		}).rejects.toThrow()
+		}).rejects.toThrow('User not found')
 	})
 })
